d087ee924a704d73d3dcec933d75dbe2f4715bbc
Simonas Viliunas <vilisimo@gmail.com>
2017-10-02 20:43:50 +0300

    Add proof of concept db connection code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,20 @@
 
 import org.apache.commons.cli.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.Optional;
 
 public class Main {
     public static void main(String args[]) throws ParseException {
+        createOptions(args);
+        connect();
+    }
+
+    private static void createOptions(String args[]) throws ParseException {
         Options options = new Options();
         options.addOption("t", false, "display current time");
 
@@ -26,12 +34,32 @@ public class Main {
         CommandLine cmd = parser.parse(options, args);
 
         if (cmd.hasOption("t")) {
-            System.out.println(new Date());
+            System.out.println("Date is: " + new Date());
         } else {
             System.out.println("No data option supplied");
         }
 
         Optional<String[]> task = Optional.ofNullable(cmd.getOptionValues("task"));
         task.ifPresent(t -> System.out.println(Arrays.toString(t)));
+    }
+
+    // Before running the program, db needs to be started:
+    // >>> java -classpath lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:hsqldb/tododb --dbname.0 tododb
+    private static void connect() {
+        Connection connection;
+
+        try {
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+            connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/tododb", "SA", "");
+
+            if (connection != null) {
+                System.out.println("Successfully established connection!");
+            } else {
+                System.out.println("Connection was not established...");
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
