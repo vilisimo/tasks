67133ba6f1fc2e1f5bb88eb6a313e5cd54e0e67c
vilisimo <vilisimo@gmail.com>
2017-10-04 21:53:11 +0300

    Add Task entity

---
+++ src/main/java/entities/Task.java
@@ -0,0 +1,18 @@
+package entities;
+
+public class Task {
+
+    private String description;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
