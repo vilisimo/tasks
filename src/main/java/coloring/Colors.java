1a8309868e2dbc489260fc5e9c1226d78ad00119
Simonas Viliunas <vilisimo@gmail.com>
2017-10-10 17:09:56 +0300

    Add yellow color

--- src/main/java/coloring/Colors.java
+++ src/main/java/coloring/Colors.java
@@ -3,6 +3,7 @@ package coloring;
 public enum Colors {
 
     ANSI_RED("\u001B[31m"),
+    ANSI_YELLOW("\u001b[33m"),
     ANSI_RESET("\u001B[0m");
 
     private final String ansi;
--- src/main/java/coloring/Printer.java
+++ src/main/java/coloring/Printer.java
@@ -1,11 +1,14 @@
 package coloring;
 
-import static coloring.Colors.ANSI_RED;
-import static coloring.Colors.ANSI_RESET;
+import static coloring.Colors.*;
 
 public class Printer {
 
     public static void error(String errorText) {
         System.out.println(ANSI_RED + errorText + ANSI_RESET);
+    }
+
+    public static void warning(String warningText) {
+        System.out.println(ANSI_YELLOW + warningText + ANSI_RESET);
     }
 }
