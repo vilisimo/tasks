4e7c2247d48cd9b26b75c1225b122e5525ef68e1
vilisimo <vilisimo@gmail.com>
2017-10-16 21:07:14 +0300

    Add green color

--- src/main/java/coloring/Colors.java
+++ src/main/java/coloring/Colors.java
@@ -4,7 +4,8 @@ public enum Colors {
 
     ANSI_RED("\u001B[31m"),
     ANSI_YELLOW("\u001b[33m"),
-    ANSI_RESET("\u001B[0m");
+    ANSI_RESET("\u001B[0m"),
+    ANSI_GREEN("\u001B[32m");
 
     private final String ansi;
 
--- src/main/java/coloring/Printer.java
+++ src/main/java/coloring/Printer.java
@@ -4,6 +4,10 @@ import static coloring.Colors.*;
 
 public class Printer {
 
+    public static void success(String successText) {
+        System.out.println(ANSI_GREEN + successText + ANSI_RESET);
+    }
+
     public static void error(String errorText) {
         System.out.println(ANSI_RED + errorText + ANSI_RESET);
     }
