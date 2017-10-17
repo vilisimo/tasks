3cf0ec580016577db058d3cbc011c1c0aab87460
vilisimo <vilisimo@gmail.com>
2017-10-17 20:51:59 +0300

    Add printer tests

---
+++ src/test/java/coloring/PrinterTest.java
@@ -0,0 +1,72 @@
+package coloring;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.assertThat;
+
+public class PrinterTest {
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStream() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void resetStream() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void successSmellsGreen() {
+        Printer.success("test");
+
+        assertThat(out.toString(), containsString(Colors.ANSI_GREEN.toString()));
+        assertThat(out.toString(), containsString("test"));
+    }
+
+    @Test
+    public void successContainsResetSequence() {
+        Printer.success("test");
+
+        assertThat(out.toString(), containsString(Colors.ANSI_RESET.toString()));
+    }
+
+    @Test
+    public void errorSmellsRed() {
+        Printer.error("test");
+
+        assertThat(out.toString(), containsString(Colors.ANSI_RED.toString()));
+        assertThat(out.toString(), containsString("test"));
+    }
+
+    @Test
+    public void errorContainsResetSequence() {
+        Printer.error("test");
+
+        assertThat(out.toString(), containsString(Colors.ANSI_RESET.toString()));
+    }
+
+
+    @Test
+    public void warningSmellsYellow() {
+        Printer.warning("test");
+
+        assertThat(out.toString(), containsString(Colors.ANSI_YELLOW.toString()));
+        assertThat(out.toString(), containsString("test"));
+    }
+
+    @Test
+    public void warningContainsResetSequence() {
+        Printer.warning("test");
+
+        assertThat(out.toString(), containsString(Colors.ANSI_RESET.toString()));
+    }
+}
